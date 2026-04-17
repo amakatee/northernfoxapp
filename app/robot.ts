2c720fd73ff50b8e8ff3fabd3c38fbc3faaae589
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2026-04-17 22:34:20 +0300

    redirect

--- app/robot.ts
+++ app/robot.ts
@@ -2,12 +2,11 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [], // Empty array means nothing is disallowed
+    },
     sitemap: "https://beihunorthernfox.com/sitemap.xml",
     host: "https://beihunorthernfox.com",
   };
